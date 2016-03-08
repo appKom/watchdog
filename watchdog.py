57a1d3cabc2d8ac3a626bdabe4ba2583d14c5d19
petterrostrup <petterrostrup@gmail.com>
2016-03-08 18:21:24 +0100

    debugging

--- watchdog.py
+++ watchdog.py
@@ -32,7 +32,7 @@ while (number != 'exit'):
 
 	dateNow = datetime.datetime.now()
 	firstWatch = dateNow.replace(hour=8, minute=0, second=0, microsecond=0)
-	lastWatch = dateNow.replace(hour=14, minute=0, second=0, microsecond=0)
+	lastWatch = dateNow.replace(hour=16, minute=0, second=0, microsecond=0)
 
 	if ((dateNow < firstWatch ) or (dateNow > lastWatch)):
 		print ("\nNot within watchhours. Checkin not registered")
